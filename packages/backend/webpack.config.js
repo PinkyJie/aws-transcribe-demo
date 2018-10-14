dae76a3476a507849db90bbd06b645abf347920a
Wenbo Jie <pinkyjie.gn@gmail.com>
2018-10-14 15:03:50 +1100

    update configuraiton
    * add prettier
    * add yarn workspaces
    * add EditorConfig

---
+++ packages/backend/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const slsw = require("serverless-webpack");
+
+module.exports = {
+    entry: slsw.lib.entries,
+    resolve: {
+        extensions: [".ts"]
+    },
+    output: {
+        libraryTarget: "commonjs",
+        path: path.join(__dirname, ".webpack"),
+        filename: "[name].js"
+    },
+    target: "node",
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            }
+        ]
+    }
+};
