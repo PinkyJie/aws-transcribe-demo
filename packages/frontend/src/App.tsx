9417077da57e4771ed0c0a094222edec42a5bb4c
Wenbo Jie <pinkyjie.gn@gmail.com>
2018-10-21 17:35:38 +1100

    basic ui

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,22 +1,57 @@
 import * as React from 'react';
+import { Header, Form, Table } from 'semantic-ui-react';
+
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Voice Assist Demo</h1>
+                </header>
+                <div className="App-body">
+                    <Header as="h2">Upload audio file</Header>
+                    <Form>
+                        <Form.Group inline>
+                            <Form.Input
+                                type="file"
+                                label="Upload audio(*.mp3)"
+                            />
+                        </Form.Group>
+                    </Form>
+                    <Header as="h2">Check transcription status</Header>
+                    <Form>
+                        <Form.Group inline>
+                            <Form.Input
+                                icon={{
+                                    name: 'search',
+                                    circular: true,
+                                    link: true,
+                                }}
+                                placeholder="Record id to search"
+                            />
+                        </Form.Group>
+                    </Form>
+                    <Table celled>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>ID</Table.HeaderCell>
+                                <Table.HeaderCell>
+                                    Audio file name
+                                </Table.HeaderCell>
+                                <Table.HeaderCell>Status</Table.HeaderCell>
+                                <Table.HeaderCell>Audio</Table.HeaderCell>
+                                <Table.HeaderCell>Text</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+
+                        <Table.Body />
+                    </Table>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
